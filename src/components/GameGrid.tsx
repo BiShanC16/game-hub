391b5b83b2f609e1c380d78dda160c18619f6fab
bcho0029 <bcho0029@student.monash.edu>
2025-08-19 00:20:18 +0800

    refractor game gric

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,9 +17,8 @@ const GameGrid = ({ gameQuery}: Props) => {
 
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>
   return (
-    <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
@@ -37,7 +36,6 @@ const GameGrid = ({ gameQuery}: Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 };
 
